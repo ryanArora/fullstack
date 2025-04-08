44e6b724b527bd834af467118fb22f588a1ef242
Ryan Arora <arora.ryan@gmail.com>
2025-04-08 13:17:17 -0400

    typecheck

--- src/server/blob.ts
+++ src/server/blob.ts
@@ -2,9 +2,8 @@ import * as Minio from "minio";
 import { env } from "~/env";
 
 export const blob = new Minio.Client({
-  endPoint: env.MINIO_HOST,
-  port: env.MINIO_PORT,
-  useSSL: env.MINIO_URL.startsWith("https://"),
+  endPoint: env.MINIO_ENDPOINT,
+  useSSL: env.MINIO_ENDPOINT.startsWith("https://"),
   accessKey: env.MINIO_USER,
   secretKey: env.MINIO_PASSWORD,
 });
