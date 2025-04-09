63b07c0eaf37e5a57188d7f6fd11ee8d5fc5defc
Ryan Arora <arora.ryan@gmail.com>
2025-04-09 14:48:59 -0400

    fix healthcehcks

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -7,19 +7,29 @@ if [ -f .env ]; then
   set +o allexport
 fi
 
-for i in {1..10}; do
-  echo "Healthcheck for postgres $i/10..."
+HEALTHCHECKS=10
 
-  if psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -c "SELECT 1;"; then
+postgres_success=false
+for i in $(seq 1 $HEALTHCHECKS); do
+  echo "Healthcheck for postgres $i/$HEALTHCHECKS..."
+
+  if psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "SELECT 1;"; then
     echo "Success on attempt $i"
+    postgres_success=true
     break
   fi
 
   sleep 2
 done
 
-for i in {1..10}; do
-  echo "Healthcheck for minio $i/10..."
+if [ "$postgres_success" = false ]; then
+  echo "Failed to connect to postgres after $HEALTHCHECKS attempts"
+  exit 1
+fi
+
+minio_success=false
+for i in $(seq 1 $HEALTHCHECKS); do
+  echo "Healthcheck for minio $i/$HEALTHCHECKS..."
 
   PROTOCOL="http"
   if [ "$MINIO_USE_SSL" = "1" ] || [ "$MINIO_USE_SSL" = "true" ]; then
@@ -28,8 +38,17 @@ for i in {1..10}; do
 
   if curl -fI "$PROTOCOL://$MINIO_ENDPOINT:$MINIO_PORT/minio/health/live"; then
     echo "Success on attempt $i"
+    minio_success=true
     break
   fi
 
   sleep 2
 done
+
+if [ "$minio_success" = false ]; then
+  echo "Failed to connect to minio after $HEALTHCHECKS attempts"
+  exit 1
+fi
+
+echo "Healthcheck successful"
+exit 0
